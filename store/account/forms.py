f44bebbcc30fa688858a91da9f3b1cfeced8a637
Amirhosein Bahrami <amirhosein.bahrami.k77@gmail.com>
2022-02-26 21:13:49 +0330

    init: login register messages template and account/forms added

--- store/account/forms.py
+++ store/account/forms.py
@@ -1,1 +1,14 @@
-from django.forms import forms
+from django import forms
+
+
+class CustomerRegistrationForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField()
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
+
+class CustomerLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField()
