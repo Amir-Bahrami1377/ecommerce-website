c636535361075372e18310fa2e2634e25eae61f4
Amirhosein Bahrami <amirhosein.bahrami.k77@gmail.com>
2022-03-01 11:56:05 +0330

    logout view added

--- store/account/urls.py
+++ store/account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from account.views import RegisterView, LoginView
+from account.views import CustomerRegisterView, CustomerLoginView, CustomerLogoutView
 
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login')
+    path('register/', CustomerRegisterView.as_view(), name='customer_register'),
+    path('login/', CustomerLoginView.as_view(), name='customer_login'),
+    path('logout/', CustomerLogoutView.as_view(), name='customer_logout')
 ]
--- store/account/views.py
+++ store/account/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from account.forms import CustomerRegistrationForm, CustomerLoginForm
@@ -5,8 +7,14 @@ from django.views.generic import View
 from account.models import Customer
 
 
-class RegisterView(View):
+class CustomerRegisterView(View):
     class_form = CustomerRegistrationForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('product:products')
+        else:
+            return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
         form = self.class_form
@@ -19,15 +27,37 @@ class RegisterView(View):
             Customer.objects.create(**cd)
             messages.success(request, 'your account created successfully', 'success')
             return redirect('product:products')
-        return redirect('accounts:register', 'account/register.html', {'form': form})
+        return render(request, 'account/register.html', {'form': form})
 
 
-class LoginView(View):
+class CustomerLoginView(View):
     class_form = CustomerLoginForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('product:products')
+        else:
+            return super().dispatch(request, *args, **kwargs)
 
     def get(self, request):
         form = self.class_form
         return render(request, 'account/login.html', {'form': form})
 
     def post(self, request):
-        pass
+        form = self.class_form(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, phone=cd['phone'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'you logged in successfully', 'success')
+                return redirect('product:products')
+            messages.error(request, 'phone or password wrong', 'warning')
+        return render(request, 'account/login.html', {'form': form})
+
+
+class CustomerLogoutView(LoginRequiredMixin, View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'you logged out successfully', 'success')
+        return redirect('product:products')
