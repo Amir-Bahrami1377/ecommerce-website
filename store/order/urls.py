dffde3ec6f3360c0f31390f77ae5152619603bb1
Amirhosein Bahrami <amirhosein.bahrami.k77@gmail.com>
2022-03-08 11:52:16 +0330

    init: order cart view and template added

--- store/order/urls.py
+++ store/order/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from order.views import CartView
 
+
+app_name = 'orders'
 urlpatterns = [
-
+    path('cart/', CartView.as_view(), name='cart')
 ]
--- store/store/urls.py
+++ store/store/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls', namespace='product')),
-    path('order/', include('order.urls')),
+    path('order/', include('order.urls', namespace='orders')),
     path('account/', include('account.urls', namespace='accounts')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
